987a55583176c1a427ef64e2b2e3c6ef8c58cfc1
Gaby <gaby@GabMac.local>
2024-05-01 21:36:46 +0100

    Test

--- meater_reader.py
+++ meater_reader.py
@@ -4,10 +4,12 @@ import datetime
 def main():
     args = parse_arguments()
     readings_list = parse_file(args.filename)
-
-
     for reading in readings_list:
         print(reading)
+    print(f"Sum of valid meters: {sum_of_valid_meters(readings_list)}")
+    print(f"Sum of invalid meters: {sum_of_invalid_meters(readings_list)}")
+    print(f"h and L: {find_highest_and_lowest_valid_reading(readings_list)}")
+    print(f"R and O: {find_most_recent_and_oldest_reading(readings_list)}")
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Read the contents of a file.")
@@ -71,18 +73,16 @@ def sum_of_invalid_meters(readings_list):
             sum +=1 # assuming its a sum of how many readings there are and not of the value of each reading
     return sum
 
-def find_highest_and_lowest_valid_reading(readings):
-    valid_readings = [reading['VALUE'] for reading in readings if reading['STATUS'] == 'V']
+def find_highest_and_lowest_valid_reading(readings_list):
+    valid_readings = [reading['VALUE'] for reading in readings_list if reading['STATUS'] == 'V']
     if not valid_readings:
         return None, None
     highest = max(valid_readings)
     lowest = min(valid_readings)
     return highest, lowest
 
-def find_most_recent_and_oldest_reading(readings):
-    valid_dates = [reading['DATE'] for reading in readings if reading['STATUS'] == 'V']
-    if not valid_dates:
-        return None, None
+def find_most_recent_and_oldest_reading(readings_list):
+    valid_dates = [reading['DATE'] for reading in readings_list]
     most_recent = max(valid_dates)
     oldest = min(valid_dates)
     return most_recent, oldest
